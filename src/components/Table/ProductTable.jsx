f467f0659f6b29f950f6baab51b6122f970bf4f8
musa genc <musagenc167@gmail.com>
2025-02-10 23:59:55 +0300

    update productTable

--- src/components/Table/ProductTable.jsx
+++ src/components/Table/ProductTable.jsx
@@ -1,24 +1,57 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
+import { useSelector } from 'react-redux';
+
+
+
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+const getRowId=(row)=>{
+    return row._id
+}
+
+export default function ProductsTable() {
+    const {products}=useSelector((state)=>state.stock)
+    console.log(products)
+
+    
+
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: '_id', headerName: 'ID', width: 90 },
   {
-    field: 'firstName',
-    headerName: 'First name',
+    field: 'categoryId',
+    headerName: 'Category',
     width: 150,
     editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
+    field: 'brandId',
+    headerName: 'Brand',
     width: 150,
     editable: true,
   },
   {
-    field: 'age',
-    headerName: 'Age',
+    field: 'name',
+    headerName: 'Name',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'quantity',
+    headerName: 'Stock',
     type: 'number',
     width: 110,
     editable: true,
@@ -33,23 +66,11 @@ const columns = [
   },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
-export default function ProductsTable() {
   return (
     <Box sx={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={rows}
+      getRowId={getRowId}
+        rows={products}
         columns={columns}
         initialState={{
           pagination: {
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,30 @@
+import { Typography } from '@mui/material'
+import { Button } from '@mui/material'
+import { Container } from '@mui/material'
 import React from 'react'
+import ProductsTable from '../components/Table/ProductsTable'
+import useStockCall from '../hook/useStockCall'
+import { useEffect } from 'react'
 
 const Products = () => {
+  const {getStockData}=useStockCall()
+
+  useEffect(()=>{
+    getStockData("products")
+    getStockData("brands")
+    getStockData("categories")
+  },[])
+
   return (
     <div>
        <Container>
-    <Typography variant="h4" color="secondary.second" align="center">
-      Products
-    </Typography>
-    <Button variant="contained"   >NEW PRODUCT</Button>
-  </Container>
-  </div>
+      <Typography variant="h4" color="secondary.second" align="center">
+        Products
+      </Typography>
+      <Button variant="contained">NEW PRODUCT</Button>
+      <ProductsTable/>
+    </Container>/
+    </div>
   )
 }
 
